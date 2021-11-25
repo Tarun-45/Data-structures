2ef2ee9771b1799cc65b9ca3be8384435a12cc57
Tarun Valluru <89525874+Tarun-45@users.noreply.github.com>
2021-11-25 20:44:00 +0530

    Add files via upload

---
+++ main (3).c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    int a[10];
+    int n;
+    int cou = 0;
+    printf("array size");
+    scanf("%d",&n);
+    printf("elements");
+    for (i=0;i<n;i+=1){
+        scanf("%d",&a[i]);
+        
+    }
+    for (i=0;i<n;i+=1){
+        if (a[i]==i){
+            cou+=1;
+        }
+    else{
+        continue;
+    }
+    }
+    if (cou>1){
+        printf("duplicates  are present");
+    }
+    else{
+        printf("no duplicates");
+    }
+    
+}
